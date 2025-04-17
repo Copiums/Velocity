b3b6293e74dde7ee6d28df341e8016f3d9588e29
Copium <119388242+Copiums@users.noreply.github.com>
2025-04-17 15:37:38 -0400

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -1,1 +1,33 @@
+--[[
 
+    ____   ____     .__                .__  __          
+    \   \ /   /____ |  |   ____   ____ |__|/  |_ ___.__.
+     \   Y   // __ \|  |  /  _ \_/ ___\|  \   __<   |  |
+      \     /\  ___/|  |_(  <_> )  \___|  ||  |  \___  |
+       \___/  \___  >____/\____/ \___  >__||__|  / ____|
+                  \/                 \/          \/      
+
+       The #1 Roblox Bedwars Vape Config on the market.
+
+        - luc - modules / organizer
+		- null.wtf - modules 
+		- copium - modules
+		- sammz - modules
+		- lr - modules
+        - blanked - modules
+        - gamesense - modules
+        - sown - modules
+        - relevant - executor
+        - nick - first UI
+
+
+
+       _  _                       _                 __ _  _       _                     _    _          
+     _| |<_> ___ ___  ___  _ _  _| |    ___  ___   / /| |<_>._ _ | |__ _ _  ___  _ _  _| |_ <_> ___ ___ 
+    / . || |<_-</ | '/ . \| '_>/ . | _ / . |/ . | / / | || || ' || / /| | |/ ._>| '_>  | |  | |<_-</ ._>
+    \___||_|/__/\_|_.\___/|_|  \___|<_>\_. |\_. |/_/  |_||_||_|_||_\_\|__/ \___.|_|    |_|  |_|/__/\___.
+                                       <___'<___'                                                       
+
+	(also I am adding comments for this file only idk why)
+]]--
+loadstring(readfile("newvape/games/Velocity.lua"))();
