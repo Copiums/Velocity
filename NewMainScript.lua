de3005ccb5d98c26f36ae6155c5df1007009ca9e
Copium <119388242+Copiums@users.noreply.github.com>
2025-04-17 12:28:39 -0400

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,59 +1,86 @@
-local isfile = isfile or function(file)
-	local suc, res = pcall(function()
-		return readfile(file)
-	end)
-	return suc and res ~= nil and res ~= ''
-end
-local delfile = delfile or function(file)
-	writefile(file, '')
-end
+--[[         _____                    _____                    _____                   _____          
+        /\    \                  /\    \                  /\    \                 /\    \         
+       /::\____\                /::\    \                /::\    \               /::\    \        
+      /:::/    /               /::::\    \              /::::\    \             /::::\    \       
+     /:::/    /               /::::::\    \            /::::::\    \           /::::::\    \      
+    /:::/    /               /:::/\:::\    \          /:::/\:::\    \         /:::/\:::\    \     
+   /:::/____/               /:::/__\:::\    \        /:::/__\:::\    \       /:::/__\:::\    \    
+   |::|    |               /::::\   \:::\    \      /::::\   \:::\    \     /::::\   \:::\    \   
+   |::|    |     _____    /::::::\   \:::\    \    /::::::\   \:::\    \   /::::::\   \:::\    \  
+   |::|    |    /\    \  /:::/\:::\   \:::\    \  /:::/\:::\   \:::\____\ /:::/\:::\   \:::\    \ 
+   |::|    |   /::\____\/:::/  \:::\   \:::\____\/:::/  \:::\   \:::|    /:::/__\:::\   \:::\____\
+   |::|    |  /:::/    /\::/    \:::\  /:::/    /\::/    \:::\  /:::|____\:::\   \:::\   \::/    /
+   |::|    | /:::/    /  \/____/ \:::\/:::/    /  \/_____/\:::\/:::/    / \:::\   \:::\   \/____/ 
+   |::|____|/:::/    /            \::::::/    /            \::::::/    /   \:::\   \:::\    \     
+   |:::::::::::/    /              \::::/    /              \::::/    /     \:::\   \:::\____\    
+   \::::::::::/____/               /:::/    /                \::/____/       \:::\   \::/    /    
+    ~~~~~~~~~~                    /:::/    /                  ~~              \:::\   \/____/     
+                                 /:::/    /                                    \:::\    \         
+                                /:::/    /                                      \:::\____\        
+                                \::/    /                                        \::/    /        
+                                 \/____/                                          \/____/         
 
-local function downloadFile(path, func)
+    The #1 Roblox Bedwars Script on the market.
+
+        - Xylex/7GrandDad - developer / organizer
+]]--
+
+local isfile: (string) -> boolean = isfile or function(file: string): boolean
+    local suc: boolean, res: string? = pcall(function(): string? return readfile(file) end);
+    return suc and res ~= nil;
+end;
+local delfile: (string) -> () = delfile or function(file: string): () writefile(file, "") end;
+
+local function downloadFile(path: string, func: ((string) -> any)?): string
 	if not isfile(path) then
-		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/Copiums/Velocity/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
-		end)
+		local suc: boolean, res: string? = pcall(function(): string
+			return game:HttpGet('https://raw.githubusercontent.com/Copiums/Velocity/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true);
+		end);
 		if not suc or res == '404: Not Found' then
-			error(res)
-		end
+			error(res);
+		end;
 		if path:find('.lua') then
-			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
-		end
-		writefile(path, res)
-	end
-	return (func or readfile)(path)
-end
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res;
+		end;
+		writefile(path, res);
+	end;
+	return (func or readfile)(path);
+end;
 
-local function wipeFolder(path)
-	if not isfolder(path) then return end
-	for _, file in listfiles(path) do
-		if file:find('loader') then continue end
+local function wipeFolder(path: string)
+	if not isfolder(path) then 
+        	return; 
+    	end;
+	for _, file: string in listfiles(path) do
+		if file:find('loader') then 
+           		 continue;
+        	end;
 		if isfile(file) and select(1, readfile(file):find('--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.')) == 1 then
-			delfile(file)
-		end
-	end
-end
+			delfile(file);
+		end;
+	end;
+end;
 
-for _, folder in {'newvape', 'newvape/games', 'newvape/profiles', 'newvape/assets', 'newvape/libraries', 'newvape/guis'} do
+for _, folder: string in {'newvape', 'newvape/games', 'newvape/profiles', 'newvape/assets', 'newvape/libraries', 'newvape/guis', 'newvape/sounds'} do
 	if not isfolder(folder) then
-		makefolder(folder)
-	end
-end
+		makefolder(folder);
+	end;
+end;
 
 if not shared.VapeDeveloper then
-	local _, subbed = pcall(function()
-		return game:HttpGet('https://github.com/Copiums/Velocity')
-	end)
-	local commit = subbed:find('currentOid')
-	commit = commit and subbed:sub(commit + 13, commit + 52) or nil
-	commit = commit and #commit == 40 and commit or 'main'
+	local _, subbed: string = pcall(function(): string
+		return game:HttpGet('https://github.com/Copiums/Velocity');
+	end);
+	local commit: string? = subbed:find('currentOid');
+	commit = commit and subbed:sub(commit + 13, commit + 52) or nil;
+	commit = commit and #commit == 40 and commit or 'main';
 	if commit == 'main' or (isfile('newvape/profiles/commit.txt') and readfile('newvape/profiles/commit.txt') or '') ~= commit then
-		wipeFolder('newvape')
-		wipeFolder('newvape/games')
-		wipeFolder('newvape/guis')
-		wipeFolder('newvape/libraries')
-	end
-	writefile('newvape/profiles/commit.txt', commit)
-end
+		wipeFolder('newvape');
+		wipeFolder('newvape/games');
+		wipeFolder('newvape/guis');
+		wipeFolder('newvape/libraries');
+	end;
+	writefile('newvape/profiles/commit.txt', commit);
+end;
 
-return loadstring(downloadFile('newvape/main.lua'), 'main')()
+return loadstring(downloadFile('newvape/main.lua'), 'main')();
