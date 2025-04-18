2a8fdaeee3e28e46bfac9aa7b4c4e8d992dd28bf
Copium <119388242+Copiums@users.noreply.github.com>
2025-04-17 23:28:09 -0400

    Update and rename New Text Document (5).txt to Velocity.lua

--- games/Velocity.lua
+++ games/Velocity.lua
@@ -1,3 +1,35 @@
+--[[
+
+    ____   ____     .__                .__  __          
+    \   \ /   /____ |  |   ____   ____ |__|/  |_ ___.__.
+     \   Y   // __ \|  |  /  _ \_/ ___\|  \   __<   |  |
+      \     /\  ___/|  |_(  <_> )  \___|  ||  |  \___  |
+       \___/  \___  >____/\____/ \___  >__||__|  / ____|
+                  \/                 \/          \/      
+
+       The #1 Roblox Bedwars Vape Config on the market.
+
+        - luc - modules / organizer
+	- null.wtf - modules 
+	- copium - modules
+	- xzxkw - modules
+	- lr - modules
+        - blanked - modules
+        - gamesense - modules
+        - sown - modules
+        - relevant - modules
+        - nick - first UI
+
+
+
+       _  _                       _                 __ _  _       _                     _    _          
+     _| |<_> ___ ___  ___  _ _  _| |    ___  ___   / /| |<_>._ _ | |__ _ _  ___  _ _  _| |_ <_> ___ ___ 
+    / . || |<_-</ | '/ . \| '_>/ . | _ / . |/ . | / / | || || ' || / /| | |/ ._>| '_>  | |  | |<_-</ ._>
+    \___||_|/__/\_|_.\___/|_|  \___|<_>\_. |\_. |/_/  |_||_||_|_||_\_\|__/ \___.|_|    |_|  |_|/__/\___.
+                                       <___'<___'                                                       
+
+]]--
+
 local velo: table = {};
 shared.nuker_range = 30;
 shared.velocity_client = true;
