9dbe48a2baf880d297f15c10d216000d035eb17c
Copium <119388242+Copiums@users.noreply.github.com>
2025-08-26 13:38:09 -0400

    Update 79695841807485.lua

--- games/79695841807485.lua
+++ games/79695841807485.lua
@@ -10,7 +10,7 @@ local starterGui = cloneref(game:GetService('StarterGui'))
 
 local gameCamera = workspace.CurrentCamera
 local lplr = playersService.LocalPlayer
-local vape = shared.vape
+local vape = shared.velo
 local entitylib = vape.Libraries.entity
 local targetinfo = vape.Libraries.targetinfo
 local prediction = vape.Libraries.prediction
