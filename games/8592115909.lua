e0e08e0068b1e6de65928adcbc91358de7ad1c8a
Copium <119388242+Copiums@users.noreply.github.com>
2025-08-26 13:52:03 -0400

    Update 8592115909.lua

--- games/8592115909.lua
+++ games/8592115909.lua
@@ -1,43 +1,45 @@
-local vape = shared.vape
+local velo = shared.velo
 local loadstring = function(...)
-	local res, err = loadstring(...)
-	if err and vape then 
-		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert') 
-	end
-	return res
-end
-local isfile = isfile or function(file)
-	local suc, res = pcall(function() 
-		return readfile(file) 
-	end)
-	return suc and res ~= nil and res ~= ''
-end
-local function downloadFile(path, func)
-	if not isfile(path) then
-		local suc, res = pcall(function() 
-			return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true) 
-		end)
-		if not suc or res == '404: Not Found' then 
-			error(res) 
-		end
-		if path:find('.lua') then 
-			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res 
-		end
-		writefile(path, res)
-	end
-	return (func or readfile)(path)
+    local res, err = loadstring(...)
+    if err and velo then 
+        velo:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert') 
+    end
+    return res
 end
 
-vape.Place = 8768229691
-if isfile('newvape/games/'..vape.Place..'.lua') then
-	loadstring(readfile('newvape/games/'..vape.Place..'.lua'), 'skywars')()
+local isfile = isfile or function(file)
+    local suc, res = pcall(function() 
+        return readfile(file) 
+    end)
+    return suc and res ~= nil and res ~= ''
+end
+
+local function downloadFile(path, func)
+    if not isfile(path) then
+        local suc, res = pcall(function() 
+            return game:HttpGet('https://raw.githubusercontent.com/Copiums/Velocity/'..readfile('velo/profiles/commit.txt')..'/'..select(1, path:gsub('velo/', '')), true) 
+        end)
+        if not suc or res == '404: Not Found' then 
+            error(res) 
+        end
+        if path:find('.lua') then 
+            res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after velocity updates.\n'..res 
+        end
+        writefile(path, res)
+    end
+    return (func or readfile)(path)
+end
+
+velo.Place = 8768229691
+if isfile('velo/games/'..velo.Place..'.lua') then
+    loadstring(readfile('velo/games/'..velo.Place..'.lua'), 'skywars')()
 else
-	if not shared.VapeDeveloper then
-		local suc, res = pcall(function() 
-			return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/games/'..vape.Place..'.lua', true) 
-		end)
-		if suc and res ~= '404: Not Found' then
-			loadstring(downloadFile('newvape/games/'..vape.Place..'.lua'), 'skywars')()
-		end
-	end
+    if not shared.VeloDeveloper then
+        local suc, res = pcall(function() 
+            return game:HttpGet('https://raw.githubusercontent.com/Copiums/Velocity/'..readfile('velo/profiles/commit.txt')..'/games/'..velo.Place..'.lua', true) 
+        end)
+        if suc and res ~= '404: Not Found' then
+            loadstring(downloadFile('velo/games/'..velo.Place..'.lua'), 'skywars')()
+        end
+    end
 end
