703027a7c792a7e9e4f0b2a475750cdfb8056ce0
Copium <119388242+Copiums@users.noreply.github.com>
2025-08-26 13:40:28 -0400

    Update 8444591321.lua

--- games/8444591321.lua
+++ games/8444591321.lua
@@ -1,9 +1,8 @@
---This watermark is used to delete the file if its cached, remove it to make the file persist after rust updates.
-local rust = shared.rust
+local vape = shared.velo
 local loadstring = function(...)
 	local res, err = loadstring(...)
-	if err and rust then 
-		rust:CreateNotification('Rust', 'Failed to load : '..err, 30, 'alert') 
+	if err and vape then 
+		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert') 
 	end
 	return res
 end
@@ -16,29 +15,29 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function() 
-			return game:HttpGet('https://raw.githubusercontent.com/0xEIite/rust/'..readfile('rust/profiles/commit.txt')..'/'..select(1, path:gsub('rust/', '')), true) 
+			return game:HttpGet('https://raw.githubusercontent.com/Copiums/Velocity/'..readfile('velo/profiles/commit.txt')..'/'..select(1, path:gsub('velo/', '')), true) 
 		end)
 		if not suc or res == '404: Not Found' then 
 			error(res) 
 		end
 		if path:find('.lua') then 
-			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after rust updates.\n'..res 
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after velocity updates.\n'..res 
 		end
 		writefile(path, res)
 	end
 	return (func or readfile)(path)
 end
 
-rust.Place = 6872274481
-if isfile('rust/games/'..rust.Place..'.lua') then
-	loadstring(readfile('rust/games/'..rust.Place..'.lua'), 'bedwars')()
+vape.Place = 6872274481
+if isfile('velo/games/'..vape.Place..'.lua') then
+	loadstring(readfile('velo/games/'..vape.Place..'.lua'), 'bedwars')()
 else
-	if not shared.RustDeveloper then
+	if not shared.VeloDeveloper then
 		local suc, res = pcall(function() 
-			return game:HttpGet('https://raw.githubusercontent.com/0xEIite/rust/'..readfile('rust/profiles/commit.txt')..'/games/'..rust.Place..'.lua', true) 
+			return game:HttpGet('https://raw.githubusercontent.com/Copiums/Velocity/'..readfile('velo/profiles/commit.txt')..'/games/'..vape.Place..'.lua', true) 
 		end)
 		if suc and res ~= '404: Not Found' then
-			loadstring(downloadFile('rust/games/'..rust.Place..'.lua'), 'bedwars')()
+			loadstring(downloadFile('velo/games/'..vape.Place..'.lua'), 'bedwars')()
 		end
 	end
 end
